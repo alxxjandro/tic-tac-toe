51472c259986ee0d42d585daf1b837e8d97aeaec
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-12-07 12:39:11 -0600

    Added the buttons in order to start implementing the game logic

--- script.js
+++ script.js
@@ -9,30 +9,55 @@ function createTile(){
 function newPlayer(name, marker){
     let playerName = name;
     let playerMarker = marker;
+    let turn = false;
 
     const tickTile = (t) => t.setValue(playerMarker);
-    return {playerName, playerMarker, tickTile};
+    return {playerName, playerMarker, tickTile, turn};
 }
 
-const gameBoard = function () {
-
-    const playerA = newPlayer("A","X");
-    const playerB = newPlayer("B","O")
-
-    //create a gameboard with 9 empty tiles;
+const createBoard = function () {
     const tiles = [];
     for (let i = 0; i < 9; i++){
         tiles.push(createTile());
     }
+    return {tiles};
+}
 
-    playerA.tickTile(tiles[0]);
-    playerB.tickTile(tiles[1]);
-    
-    for (let i = 0; i < 9; i++){
-        console.log(tiles[i].getValue());
-    }
+const printBoard = function (game){
+    console.log(game.tiles[0].getValue(), game.tiles[1].getValue(), game.tiles[2].getValue());
+    console.log(game.tiles[3].getValue(), game.tiles[4].getValue(), game.tiles[5].getValue());
+    console.log(game.tiles[6].getValue(), game.tiles[7].getValue(), game.tiles[8].getValue());
+}
 
+const switchTurns = function (players){
+    players.forEach(player =>{
+        player.turn = !player.turn;
+    })
+}
+
+const startGame = function () {
+    const game = createBoard();
+    console.log(game)
+    const players = [newPlayer("p1","X"),newPlayer("p2","O")];
+    players[0].turn = true;
+
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button =>{
+        button.addEventListener('click', () =>{
+            if (game.tiles[button.textContent].getValue() != null){
+                alert("Already filled");
+            } else{
+                if(players[0].turn){ //if true
+                    players[0].tickTile(game.tiles[button.textContent]);
+                    switchTurns(players);
+                    printBoard(game);
+                } else {
+                    players[1].tickTile(game.tiles[button.textContent]);
+                    switchTurns(players);
+                    printBoard(game);
+                }
+            }
+        })
+    })  
 } ();
-
-
 
